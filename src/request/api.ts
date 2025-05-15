249649b60206e9fd5b170171e64dc4d7390f2a22
jianyi <jianyi@codejoy.ai>
2025-05-15 18:33:01 +0800

    feat(api): 新增保存文章、收藏诗词、取消收藏和获取收藏列表的API接口
    添加了四个新的API接口，用于处理文章的保存、诗词的收藏与取消收藏，以及获取收藏列表。这些接口将支持前端相关功能的实现。

--- src/request/api.ts
+++ src/request/api.ts
@@ -24,3 +24,67 @@ export const generatePoemApi = (data: {
     },
   });
 };
+
+//保存文章
+export const savePoemApi = (data: {
+  toolName: string;
+  toolType: string;
+  setting: string;
+}) => {
+  const userId = localStorage.getItem("userId") || "test1";
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/createLog",
+    // url: "http://localhost:4040/api/tool/createLog",
+    data: data,
+    headers: {
+      platform: "lndx",
+      userId: userId,
+    },
+  });
+};
+
+// 收藏诗词
+export const collectPoemApi = (data: { uuid: string }) => {
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/collection",
+    // url: "http://localhost:4040/api/tool/collection",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// 取消收藏
+export const cancelCollectPoemApi = (data: { uuid: string }) => {
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/cancelCollection",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// 获取收藏的列表
+export const getCollectPoemListApi = (data: {
+  page: number;
+  pageSize: number;
+  toolName: string;
+}) => {
+  const userId = localStorage.getItem("userId") || "test1";
+
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/collectionList",
+    // url: "http://localhost:4040/api/tool/collectionList",
+    data: data,
+    headers: {
+      platform: "lndx",
+      userId: userId,
+    },
+  });
+};
